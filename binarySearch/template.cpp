ec826e38fa727c226ccd4bdc80bb19c70560243a
Chinmay Jain <157116780+CHINMAYJAI@users.noreply.github.com>
2026-02-15 18:23:04 +0530

    another way to calculate mid
    mid can be calculated using bitwise operator also

--- binarySearch/template.cpp
+++ binarySearch/template.cpp
@@ -2,6 +2,7 @@ int binary_search(vector<int>& arr, int x) {
     int left = 0, right = a.size() - 1;
     int mid;
     while (left <= right) {
+        // another ways to calc mid -> `(left+right)/2` or `(left+right)>>1`
         mid = left + (right - left) / 2;
         if (arr[mid] == x) break;
         else if (arr[mid] < x) left = mid + 1;
