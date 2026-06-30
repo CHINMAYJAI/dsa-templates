bf2a505f97a46c89c03ddcaa55450195a3da69af
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-06-30 17:52:00 +0530

    added: kadanesAlgo.cpp

---
+++ arrayAlgo/kadanesAlgo.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kadanesAlgo(vector<int>nums) {
+    int maxi=INT_MIN;
+    int sum=0;
+    for (int i=0;i<nums.size();i++) {
+        sum+=nums[i];
+        if (maxi<sum) maxi=sum;
+        if (sum<0) sum=0;
+    }
+    return maxi;
+}
